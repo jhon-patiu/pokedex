1c6cdf3fe932705e0af7ff110313da7587d5e319
Jhon Patiu <patiunohj@gmail.com>
2021-12-07 11:22:54 +0800

    add styles and functionality

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
+"use strict";
+
 const btn = document.querySelector(".btn");
 const inputSearch = document.querySelector(".input-search");
 const pokemonDisplay = document.querySelector(".pokemon");
 
-const fetchData = async (query) => {
-    // url
-    const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+window.addEventListener("DOMContentLoaded", () => {
+    if (inputSearch.value === "") {
+        fetchData("charmander");
+    }
+});
 
+const fetchData = async (query) => {
+    //url
+    let url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+
+    // fetch
     const res = await fetch(url);
+
+    if (!res.ok) {
+        pokemonDisplay.innerHTML = `
+        <h2 class="error">ERROR<span>404</span>: Pokemon Not Found!</h2>
+       `;
+    }
 
     const data = await res.json();
 
     // set pokemon
     const pokemon = {
-        sprite: data.sprites.other.home.front_default,
         name: data.name,
+        sprite: data.sprites.other.home.front_default,
         type: data.types.map((type) => type.type.name).join(", "),
         stats: data.stats
             .map(
-                (stat) =>
-                    `<li>${stat.stat.name + " - " + stat["base_stat"]}</li>`
+                (stat) => `<li>${stat.stat.name + " - " + stat.base_stat}</li>`
             )
             .join(""),
-        height: data.height,
-        weight: data.weight,
     };
 
-    // display to HTML
+    console.log(pokemon);
+
+    // display to html
     pokemonDisplay.innerHTML = `
-    <div class="pokemon__model-bg">
-    <img src=${pokemon.sprite} alt=${pokemon.name} class="pokemon__model" />
-    </div>
-        
-        
+        <img src=${pokemon.sprite} alt=${pokemon.name} class="pokemon__model"/>
         <div class="pokemon__info">
-            <h2 class="pokemon__info-name">${pokemon.name}</h2>     
-            
-            <div class="pokemon__info-stats">
-             <p>type: ${pokemon.type}</p>
-            
-                <ul>
-                  ${pokemon.stats}
-                </ul>
-            </div>
+        <h2>${pokemon.name}</h2>
+        <p>Type: ${pokemon.type}</p>
+        <div class="pokemon__info-stats">
+             <ul>
+              ${pokemon.stats}
+             </ul>
         </div>
+       
+       
+        
 
     `;
 };
-fetchData();
 
-btn.addEventListener("click", fetchData(inputSearch.value));
+btn.addEventListener("click", () => fetchData(inputSearch.value));
